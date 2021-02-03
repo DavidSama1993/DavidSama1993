aa8340c7e63157de4b9f022c7e4c060fec092615
DavidSama1993 <18210222071@163.com>
2021-02-03 18:24:12 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <div class="main"></div>
+    <div class="main">
+      <div class="header clear">
+        <div class="logo float-left">DAVID SAMA</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,9 +22,30 @@ export default class App extends Vue {}
 
 <style lang="scss" scoped>
 .main {
-  height: 100%;
-  margin: 0 auto;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: -5px auto;
+  padding: 5px 0;
   width: 800px;
+  //background: rgba(48, 48, 48, 0.1);
+  box-shadow: 0 0 30px rgba(48, 48, 48, 0.8), 0 0 3px rgba(48, 48, 48, 0.8), -10px 0 5px #fff;
+  .header {
+    height: 60px;
+    border-bottom: 1px solid #eee;
+    .logo {
+      line-height: 60px;
+      color: rgba(200, 210, 220, 0.5);
+      text-shadow: 5px 0 5px #000, -1px -1px 5px #abc;
+      font-size: 50px;
+      font-weight: bold;
+      font-style: italic;
+      font-family: monospace;
+      word-spacing: -18px;
+    }
+  }
 }
 @media only screen and (min-width: 1000px) {
   .main {
